931a8b758b8b77a5fd1d96d31cdce69beef787dc
karenchloec <karenchloec@users.noreply.github.com>
2024-03-14 21:09:59 -0500

    button:hover > jquery function, logo scroll resize
    changing css animation into a jquery function, logo at the top is large but when scrolling it becomes smaller scale so that it fits on the header that is not fixated to the top of the screen even when scrolling

--- js/index.js
+++ js/index.js
@@ -8,3 +8,52 @@ $(document).ready(function() {
     });
 });
 
+
+
+//this seems a lot more complicated than doing the css version of it
+$(document).ready(function() {
+    $('button').hover(
+        function() { 
+            $(this).css({
+                'background-color': '#C3F0E8',
+                'color': 'black'
+            });
+        },
+        function() { 
+            $(this).css({
+                'background-color': 'transparent',
+                'color': '#EDE7E2'
+            });
+        }
+    );
+});
+
+
+$(document).ready(function() {
+
+    var initialScale = 2.5;
+ 
+    var maxScale = 2; 
+    var minScale = 1; 
+
+    var documentHeight = $(document).height();
+    var viewportHeight = $(window).height();
+    var maxScroll = documentHeight - viewportHeight;
+
+    $(window).scroll(function() {
+        var scrollTop = $(this).scrollTop();
+
+     
+        var newScale = initialScale - (scrollTop * 0.01); 
+        newScale = Math.min(newScale, maxScale);
+        newScale = Math.max(newScale, minScale);
+
+        $('#logoImg img').css({
+            'transform': 'scale(' + newScale + ')',
+            'transform-origin': 'top left'
+        });
+    });
+});
+
+
+
